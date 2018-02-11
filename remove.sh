4f7d32482e391c88fcfa00f4eace745b9b2fde34
zhq <32982289+zhq1@users.noreply.github.com>
2018-02-11 12:16:26 +0800

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo service nginx stop
+sudo service mysql stop
+sudo service php7.0-fpm stop
 killall -9 main.sh
 killall -9 run.sh
 killall -9 loop2.sh
@@ -6,7 +9,8 @@ killall -9 blocks.sh
 killall -9 php
 killall -9 mysql
 killall -9 nginx
-	sudo service nginx stop
+tar -cvjpf mycnfbak.tar.gz $HOME/.my.cnf
+cd /var/lib/mysql && tar -cvjpf mysqlbak.tar.gz /var/lib/mysql/*
 	sudo aptitude -y remove nginx
 	sudo aptitude -y remove php7.0-fpm
 	sudo aptitude -y remove php7.0-opcache php7.0-fpm php7.0 php7.0-common php7.0-gd php7.0-mysql php7.0-imap php7.0-cli php7.0-cgi php-pear php-auth php7.0-mcrypt mcrypt imagemagick libruby php7.0-curl php7.0-intl php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl memcached php-memcache php-imagick php-gettext php7.0-zip php7.0-mbstring
@@ -33,4 +37,3 @@ rm -rf /$HOME/backup/*
 rm -rf /var/web
 rm -rf /var/www/*
 rm -rf /var/stratum
-rm -rf /var/lib/mysql/*
