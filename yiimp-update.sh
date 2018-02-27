62c3b95f6c7b74405b833cbfe561229a99ad4429
zhq <32982289+zhq1@users.noreply.github.com>
2018-02-27 17:38:54 +0800

    Update yiimp-update.sh

--- yiimp-update.sh
+++ yiimp-update.sh
@@ -6,10 +6,10 @@ killall -9 main.sh
 killall -9 run.sh
 killall -9 loop2.sh
 killall -9 blocks.sh
-tar zcvf stratumbak.tar.gz /var/stratum/*
-tar zcvf webbak.tar.gz /var/web/*
-tar zcvf mysqlbak.tar.gz /var/lib/mysql/*
-tar zcvf mysqlbak.tar.gz /usr/lib/mysql/*
+tar -cvjpf stratumbak.tar.gz /var/stratum/*
+tar -cvjpf webbak.tar.gz /var/web/*
+tar -cvjpf mysqlbak.tar.gz /var/lib/mysql/*
+tar -cvjpf mysqlbak.tar.gz /usr/lib/mysql/*
 sudo service mysql start
 
 cd $HOME/
@@ -30,7 +30,7 @@ rm -rf /var/web/yaamp
 rm -rf /var/web/images
 rm -rf /var/web/framework
 rm -rf /var/web/extensions
-tar zcvf stratumbak.tar.gz /var/stratum/stratum
+tar -cvjpf stratumbak.tar.gz /var/stratum/stratum
 
 sudo cp -r $HOME/yiimp/web/yaamp /var/web/yaamp
 sudo cp -r $HOME/yiimp/web/images /var/web/images
@@ -39,4 +39,22 @@ sudo cp -r $HOME/yiimp/web/extensions /var/web/extensions
 sudo cp -r $HOME/yiimp/stratum/stratum /var/stratum/
 rm -rf /var/stratum/*.log
 rm -rf /var/stratum/config/*.log
+
+
+cd ~
+cd $HOME/yiimp/sql
+sudo mysql --defaults-group-suffix=host1 --force < 2017-11-segwit.sql
+sudo mysql --defaults-group-suffix=host1 --force < 2018-01-stratums_ports.sql
+sudo mysql --defaults-group-suffix=host1 --force < 2018-02-coins_getinfo.sql
+
+
+sudo chmod -R 775 /var/www/$server_name/html
+sudo chmod -R 775 /var/web
+sudo chmod -R 775 /var/stratum
+sudo chmod -R 775 /var/web/yaamp/runtime
+sudo chmod -R 664 /root/backup/
+sudo chmod -R 644 /var/log/debug.log
+sudo chmod -R 775 /var/web/serverconfig.php
+sudo service nginx restart
+sudo service php7.0-fpm reload
 
